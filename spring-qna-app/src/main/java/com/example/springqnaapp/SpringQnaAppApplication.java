1576893f13e30100efc298b179ca400266b7ae0b
seonghun120614 <seonghun120614@gmail.com>
2025-12-22 15:01:38 +0900

    ⚙️  설정: #1 초기 프로젝트 설정

---
+++ spring-qna-app/src/main/java/com/example/springqnaapp/SpringQnaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springqnaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQnaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQnaAppApplication.class, args);
+	}
+
+}
---
+++ spring-qna-app/src/test/java/com/example/springqnaapp/SpringQnaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springqnaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQnaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
