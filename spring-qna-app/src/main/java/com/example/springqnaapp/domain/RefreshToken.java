42b5597b99a7b9f48287fc4d80d2b2af5fd1a3a0
seonghun120614 <seonghun120614@gmail.com>
2025-12-22 16:17:14 +0900

    ⚙️  설정: `RefreshToken` 엔티티 추가

---
+++ spring-qna-app/src/main/java/com/example/springqnaapp/domain/RefreshToken.java
@@ -0,0 +1,31 @@
+package com.example.springqnaapp.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "refresh_token")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class RefreshToken {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "user_id")
+	private Long userId;
+
+	@Column(name = "value")
+	private String value;
+}
