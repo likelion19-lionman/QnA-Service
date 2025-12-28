2750935458680ea39493a925f63d4a90dc83f657
seonghun120614 <seonghun120614@gmail.com>
2025-12-28 22:03:13 +0900

    🐞 픽스: 오타 수정

--- spring-qna-app/src/main/java/com/example/springqnaapp/config/SecurityConfig.java
+++ spring-qna-app/src/main/java/com/example/springqnaapp/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
 		return http
 				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/auth/**")
+						.requestMatchers("/api/auth/**")
 						.permitAll()
 						.anyRequest()
 						.authenticated()
