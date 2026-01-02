c8270436c3c1f80e35d273d7b63ecb0e79c46806
Kim Jaehun <hun970412@naver.com>
2026-01-02 21:01:49 +0900

    ✨ 기능: #41 로그아웃 프론트 최초 구현
    - 204 No Content 응답 처리 설정
    - 정상 로그아웃 처리 완료 확인

--- nextjs-qna-app/app/api/auth.js
+++ nextjs-qna-app/app/api/auth.js
@@ -1,95 +1,113 @@
-import { baseRequest } from './api';
+import { baseRequest } from "./api";
 
-export async function checkDuplication(
-    username
-) {
-    return await baseRequest(
-        '/auth/check-duplication',
-        'POST',
-        {
-            'Content-Type': 'text/plain',
-            'Accept': 'application/json'
-        },
-        {
-            username: username
-        }
-    )
+export async function checkDuplication(username) {
+  return await baseRequest(
+    "/auth/check-duplication",
+    "POST",
+    {
+      "Content-Type": "text/plain",
+      Accept: "application/json",
+    },
+    {
+      username: username,
+    }
+  );
 }
 
-export async function register(
-    username,
-    email,
-    password
-) {
-    const res = await baseRequest(
-        '/auth/register',
-        'POST',
-        {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        {
-            username: username,
-            email: email,
-            password: password
-        },
-        `회원가입 불가`
-    );
+export async function register(username, email, password) {
+  const res = await baseRequest(
+    "/auth/register",
+    "POST",
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    {
+      username: username,
+      email: email,
+      password: password,
+    },
+    `회원가입 불가`
+  );
 
-    if (res && res.refreshToken) {
-        console.log("✅ 회원가입 완료 및 refreshToken 저장");
-        localStorage.setItem('refreshToken', res.refreshToken);
-    }
+  if (res && res.refreshToken) {
+    console.log("✅ 회원가입 완료 및 refreshToken 저장");
+    localStorage.setItem("refreshToken", res.refreshToken);
+  }
 }
 
 export async function login(username, password) {
-    const res = await baseRequest(
-        '/auth/login',
-        'POST',
-        {
-            'Content-Type': 'application/json',
-            'Accept': 'text/plain'
-        },
-        {
-            'username': username,
-            'password': password
-        },
-        `${username} 로그인 실패`
-    );
+  const res = await baseRequest(
+    "/auth/login",
+    "POST",
+    {
+      "Content-Type": "application/json",
+      Accept: "text/plain",
+    },
+    {
+      username: username,
+      password: password,
+    },
+    `${username} 로그인 실패`
+  );
 
-    if (res) {
-        console.log("✅ 로그인 완료 및 refreshToken -> LocalStorage 저장");
-        localStorage.setItem('refreshToken', res);
-    }
+  if (res) {
+    console.log("✅ 로그인 완료 및 refreshToken -> LocalStorage 저장");
+    localStorage.setItem("refreshToken", res);
+  }
 }
 
 export async function requestAuthCode(email) {
-    return await baseRequest(
-        '/auth/email/send',
-        'POST',
-        {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        {
-            'email': email
-        },
-        `${email} 이메일 전송 요청 실패`
-    );
+  return await baseRequest(
+    "/auth/email/send",
+    "POST",
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    {
+      email: email,
+    },
+    `${email} 이메일 전송 요청 실패`
+  );
 }
 
 export async function validateAuthCode(email, code) {
-    return await baseRequest(
-        '/auth/email/verify',
-        'POST',
-        {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        {
-            'email': email,
-            'code': code
-        },
-        `${email} 이메일 인증 실패`
-    );
+  return await baseRequest(
+    "/auth/email/verify",
+    "POST",
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    {
+      email: email,
+      code: code,
+    },
+    `${email} 이메일 인증 실패`
+  );
+}
+
+export async function logout() {
+  const refreshToken = localStorage.getItem("refreshToken");
+
+  if (!refreshToken) {
+    alert("이미 로그아웃 상태입니다.");
+    window.location.href = "/";
+    return;
+  }
+
+  localStorage.removeItem("refreshToken");
+
+  await baseRequest(
+    "/auth/logout",
+    "POST",
+    {
+      "Content-Type": "application/json",
+    },
+    {
+      refreshToken: refreshToken,
+    },
+    "로그아웃 실패"
+  );
 }
--- nextjs-qna-app/app/page.js
+++ nextjs-qna-app/app/page.js
@@ -1,38 +1,38 @@
-'use client'
+"use client";
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
+import { logout } from "@/app/api/auth";
 
 export default function HomePage() {
-	const router = useRouter();
+  const router = useRouter();
 
-	return <>
-		<div>
-			<h1>홈페이지</h1>
-			<div>
-				<button 
-					onClick={() => router.push('/auth/login')}
-				>
-					로그인
-				</button>
-				
-				<button 
-					onClick={() => router.push('/auth/register')}
-				>
-					회원가입
-				</button>
-				
-				<button 
-					// onClick={() => handleLogout()}
-				>
-					로그아웃
-				</button>
-				
-				<button 
-					onClick={() => router.push('/qna')}
-				>
-					QnA
-				</button>
-			</div>
-		</div>
-	</>
+  const handleLogout = async () => {
+    try {
+      await logout();
+      alert("로그아웃 성공");
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      router.push("/");
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h1>홈페이지</h1>
+        <div>
+          <button onClick={() => router.push("/auth/login")}>로그인</button>
+
+          <button onClick={() => router.push("/auth/register")}>
+            회원가입
+          </button>
+
+          <button onClick={() => handleLogout()}>로그아웃</button>
+
+          <button onClick={() => router.push("/qna")}>QnA</button>
+        </div>
+      </div>
+    </>
+  );
 }
